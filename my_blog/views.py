8db8b9e066491fed70d1409e6c9a79481b2870db
yuxiong.wangyx <yuxiong.wangyx@alibaba-inc.com>
2018-07-31 14:48:50 +0800

    http -> https

--- my_blog/views.py
+++ my_blog/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render, render_to_response
 def handler404(request):
     return render(request, '404.html', status=404)
 
+
 def referral(request):
     return render_to_response('referral.html')
